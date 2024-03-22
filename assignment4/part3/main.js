aec23999f6c6c7740acbf2f0200b1fd42bda8f46
abdullahl12 <156445009+abdullahl12@users.noreply.github.com>
2024-03-22 10:16:24 -0400

    Added comments

--- assignment4/part3/main.js
+++ assignment4/part3/main.js
@@ -23,12 +23,14 @@ class Ball {
       this.color = color;
       this.size = size;
     }
+    
     draw() {
         ctx.beginPath();
         ctx.fillStyle = this.color;
         ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
         ctx.fill();
       }
+
     update() {
         if ((this.x + this.size) >= width) {
           this.velX = -(this.velX);
@@ -64,21 +66,23 @@ class Ball {
       }    
   }
 
-const balls = [];
+  const balls = [];
 
-while (balls.length < 25) {
-  const size = random(10, 20);
-  const ball = new Ball(
-    random(0 + size, width - size),
-    random(0 + size, height - size),
-    random(-5, 5),
-    random(-5, 5),
-    randomRGB(),
-    size,
-  );
-
-  balls.push(ball);
-}
+  while (balls.length < 25) {
+    const size = random(10, 20);
+    const ball = new Ball(
+      // ball position always drawn at least one ball width
+      // away from the edge of the canvas, to avoid drawing errors
+      random(0 + size, width - size),
+      random(0 + size, height - size),
+      random(-7, 7),
+      random(-7, 7),
+      randomRGB(),
+      size,
+    );
+  
+    balls.push(ball);
+  }
 function loop() {
     ctx.fillStyle = "rgb(0 0 0 / 25%)";
     ctx.fillRect(0, 0, width, height);
