f823cd025a3d97fcf7a7906ab47f62b3056609da
abdullahl12 <156445009+abdullahl12@users.noreply.github.com>
2024-04-01 15:07:05 -0400

    Added main.js in part 2 with the comments

--- assignment4/part2/gallery-start/main.js
+++ assignment4/part2/gallery-start/main.js
@@ -1,18 +1,44 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
-/* Declaring the alternative text for each image file */
-
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const alts = {
+  'pic1.jpg' : ' humans eye',
+  'pic2.jpg' : 'Wave',
+  'pic3.jpg' : 'Purple flowers',
+  'pic4.jpg' : 'wall image',
+  'pic5.jpg' : 'insect on leaf'
+}
 /* Looping through images */
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('alt', alts[image]);
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+  thumbBar.appendChild(newImage);
+
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+
+    displayedImage.alt = e.target.alt;
+  });
+}
 
 /* Wiring up the Darken/Lighten button */
+// Lighten it
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    // Darken it
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
